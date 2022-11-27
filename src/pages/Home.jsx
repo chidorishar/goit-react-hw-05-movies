0ae9d27c9afabe8afb9ae8d94c154ce2251c7f33
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-27 21:45:10 +0200

    Added Home component markup and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,30 @@
-export function Home(props) {
-  return null;
+import { Container } from 'components/common/shared.styled';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getTrendingMovies } from 'services/MovieAPI';
+
+export function Home() {
+  const [moviesData, setMoviesData] = useState(null);
+
+  useEffect(() => {
+    const fetchMoviesData = async () =>
+      setMoviesData(await getTrendingMovies());
+
+    fetchMoviesData();
+  }, []);
+
+  return (
+    <Container>
+      <h2>Trending today</h2>
+      {moviesData && (
+        <ul>
+          {moviesData.map(({ title, id, original_name }) => (
+            <li key={id}>
+              <Link to={`movies/${id}`}>{title ?? original_name}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Container>
+  );
 }
