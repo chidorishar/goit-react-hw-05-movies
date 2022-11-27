b0110bdb0f3198c1126ccd37ac8d39e773d8bd55
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-27 21:42:35 +0200

    Added SharedLayout component markup and styles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,8 +1,27 @@
+import { Container } from 'components/common/shared.styled';
 import { Outlet } from 'react-router-dom';
+import { Header, HeaderLink, LinksList } from './SharedLayout.styled';
+
+const LINKS = [
+  { name: 'Home', to: '/' },
+  { name: 'Movies', to: 'movies' },
+];
 
 export function SharedLayout(props) {
   return (
     <>
+      <Header>
+        <Container>
+          <LinksList>
+            {LINKS.map(({ to, name }) => (
+              <HeaderLink to={to} key={name}>
+                {name}
+              </HeaderLink>
+            ))}
+          </LinksList>
+        </Container>
+      </Header>
+
       <Outlet />
     </>
   );
---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  width: 100%;
+  padding: ${({ theme: { space } }) => space[3]}px;
+
+  background-color: ${({ theme: { colors } }) => colors.accentSecondary};
+  border-bottom: 2px solid;
+`;
+
+export const LinksList = styled.ul`
+  display: inline-block;
+
+  padding: ${({ theme: { space } }) => space[2]}px;
+  border-radius: ${({ theme: { radii } }) => radii.big};
+
+  background-color: ${({ theme: { colors } }) => colors.light};
+`;
+
+export const HeaderLink = styled(NavLink)`
+  font-size: ${({ theme: { fontSizes } }) => fontSizes[4]}px;
+  font-weight: ${({ theme: { fontWeights } }) => fontWeights.medium};
+  color: black;
+
+  &:not(:last-child) {
+    margin-right: ${({ theme: { space } }) => space[3]}px;
+  }
+
+  &.active {
+    color: ${({ theme: { colors } }) => colors.textColored};
+    text-decoration: underline;
+  }
+`;
