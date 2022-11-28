be424bfc7bc176c76e03c119eb2daa8363ef9dff
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-28 11:13:59 +0200

    Added Reviews component`s markup

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,3 +1,35 @@
-export function Reviews(props) {
-  return null;
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getMovieReviewsByID } from 'services/MovieAPI';
+
+export function Reviews() {
+  const { movieId } = useParams();
+  const [movieReviews, setMovieReviews] = useState(null);
+
+  useEffect(() => {
+    async function getMovieReviews() {
+      setMovieReviews(await getMovieReviewsByID(movieId));
+    }
+
+    getMovieReviews();
+  }, [movieId]);
+
+  //no data - no markup
+  if (!movieReviews) return null;
+
+  return (
+    <ul>
+      {movieReviews.length > 0
+        ? movieReviews.map(({ author, content, id }) => (
+            <li key={id}>
+              <p>
+                <b>Author: {author}</b>
+              </p>
+              <p>{content}</p>
+            </li>
+          ))
+        : 'We don`t have any reviews for this movie!'}
+    </ul>
+  );
 }
