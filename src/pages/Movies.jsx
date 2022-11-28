bda0c49f808c0f14f9cd89815d556680f5545dc5
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-28 18:23:22 +0200

    Added Movies component logic and markup

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,3 +1,51 @@
-export function Movies(props) {
-  return null;
+import { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
+import { Container } from 'components/common/shared.styled';
+import { MoviesList } from 'components/common/MoviesList/MoviesList';
+
+import { getMoviesByName } from 'services/MovieAPI';
+
+const SEARCH_QUERY_PARAM_KEY = 'query';
+
+export function Movies() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [foundedMovies, setFoundedMovies] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    async function fetchMoviesByName() {
+      setFoundedMovies(await getMoviesByName(searchedMovieName));
+    }
+
+    const searchedMovieName = searchParams.get(SEARCH_QUERY_PARAM_KEY);
+    searchedMovieName ? fetchMoviesByName() : setFoundedMovies(null);
+  }, [searchParams]);
+
+  const onSearchFormSubmit = e => {
+    e.preventDefault();
+    const searchQuery = e.target.searchQuery.value;
+
+    if (!searchQuery) {
+      setSearchParams({});
+      return;
+    }
+
+    setSearchParams({
+      [SEARCH_QUERY_PARAM_KEY]: searchQuery,
+    });
+    e.target.reset();
+  };
+
+  return (
+    <Container>
+      <form onSubmit={onSearchFormSubmit}>
+        <input name="searchQuery" />
+        <button type="submit">Search</button>
+      </form>
+      {foundedMovies && (
+        <MoviesList moviesData={foundedMovies} location={location} />
+      )}
+    </Container>
+  );
 }
