bdac7c77af4fd1c0762b723070dcaf2639c4895b
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-11-28 11:54:03 +0200

    Added markup and logic for Cast component

--- src/pages/Cast.jsx
+++ src/pages/Cast.jsx
@@ -1,3 +1,38 @@
-export function Cast(props) {
-  return null;
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getMovieCastByID } from 'services/MovieAPI';
+
+export function Cast() {
+  const { movieId } = useParams();
+  const [movieCast, setMovieCast] = useState(null);
+
+  useEffect(() => {
+    async function getMovieCast() {
+      setMovieCast(await getMovieCastByID(movieId));
+    }
+
+    getMovieCast();
+  }, [movieId]);
+
+  //no data - no markup
+  if (!movieCast) return null;
+
+  return (
+    <ul>
+      {movieCast.length
+        ? movieCast.map(({ name, character, profile_path, id }) => (
+            <li key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w300/${profile_path}`}
+                alt={`${name}`}
+                width="150"
+              />
+              <p>{name}</p>
+              <p>Character: {character}</p>
+            </li>
+          ))
+        : 'We don`t have any data about cast for this movie!'}
+    </ul>
+  );
 }
